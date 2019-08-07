09a2d18047069e754f37e01902e8a4d0c1f02130
xo28122000 <jainam.2000@yahoo.com>
2019-08-07 11:02:59 -0700

    week 3 completer

--- courseera - intoduction to swift programming/week 3 - 1.playground/Contents.swift
+++ courseera - intoduction to swift programming/week 3 - 1.playground/Contents.swift
@@ -2,13 +2,17 @@ import UIKit
 
 var str = "Hello, playground"
 
+
+
+
+//------------------------------------------------------------------------------------
 var maybestr : String? = nil
 
 print(maybestr)
 //print(maybestr!)
 
 
-
+//------------------------------------------------------------------------------------
 
 func performsomething(spell:String)->String{
     print("\(spell) is magical")
@@ -25,6 +29,14 @@ var someotherfunction = {
     return(spell)
      }
 
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------
 struct Animal{
     var name = ""
     var heightinches=0.0
@@ -53,6 +65,116 @@ dog.heightcm
 
 
 
-
+//------------------------------------------------------------------------------------
 let noValue:Int? = nil
-let unwrappedValue = noValue!
+//let unwrappedValue = noValue!
+
+
+
+//------------------------------------------------------------------------------------
+
+var a=5
+var b = a
+b=3
+a
+
+
+class number1{
+    var value:Int
+    init(value:Int) {
+        self.value = value
+    }
+}
+
+var anum = number1(value: 3)
+var bnum = anum
+bnum.value=5
+anum.value
+
+
+struct number2{
+    var value:Int
+    init(value:Int) {
+        self.value = value
+    }
+}
+
+var anum2 = number2(value: 3)
+var bnum2 = anum2
+bnum2.value=5
+anum2.value
+
+
+
+
+//------------------------------------------------------------------------------------
+
+
+
+
+class someclass{
+    var value:Int
+    required init(value:Int) {
+        self.value = value
+    }
+//    convinience init would be called if no parameters were called
+    convenience init(){
+        self.init(value:1)
+    }
+    func getsomehing()->Int{
+    return 5
+    }
+}
+
+//this class inherits the previous class
+class some_inhereted_class : someclass{
+    override func getsomehing() -> Int {
+     return 3
+    }
+}
+
+//extension is used to add more func to an existing class
+extension someclass{
+    func getsomehingmore()->Int{
+        return 100
+    }
+}
+
+
+//inteface in java is protocol in swift, it defines how a class should be structured
+protocol someprotocol{
+    func somefuncthathastobeimplemented()
+}
+
+class protocoledclass: someprotocol{
+    func somefuncthathastobeimplemented() {
+        
+    }
+}
+
+class protocollaedandinheretedclass :  someclass, someprotocol{
+    func somefuncthathastobeimplemented() {
+        
+    }
+}
+
+
+
+
+
+
+//------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+enum typesofvegetables: Int{
+    case tomatoes
+    case carrots
+//    case one=1,2
+}
+typesofvegetables.tomatoes
